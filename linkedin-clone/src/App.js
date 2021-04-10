8df66e11e4b7e38fdc05dd60c2ae1e0b8ca92299
indalkumaryadav <yadavindal269@gmail.com>
2021-04-10 13:45:14 +0530

    linkedin clone

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
-      <h1></h1>
+      <Home />
     </>
   );
 };
---
+++ linkedin-clone/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/header/Header";
+import SideBar from "../components/sidebar/SideBar";
+
+const Home = () => {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
+      <SideBar />
+    </>
+  );
+};
+
+export default Home;
